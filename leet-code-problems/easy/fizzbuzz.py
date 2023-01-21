3ce7b975a072a4956e922ca7c8c46970cb452f6f
Quinn Le <quynhnle135@gmail.com>
2023-01-21 12:33:49 -0500

    FizzBuzz

---
+++ leet-code-problems/easy/fizzbuzz.py
@@ -0,0 +1,19 @@
+class Solution:
+    def fizzBuzz(self, n):
+        result = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            else:
+                result.append(str(i))
+        return result
+
+
+my_solution = Solution()
+print(my_solution.fizzBuzz(3))
+print(my_solution.fizzBuzz(5))
+print(my_solution.fizzBuzz(15))
